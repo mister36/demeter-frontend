7ebed3b45c7ef4353104e7c33773124084bdd95d
Adam Achebe <mister363636@gmail.com>
2023-03-04 17:13:50 -0700

    Home init

---
+++ src/Home.js
@@ -0,0 +1,231 @@
+import "./index.css";
+
+const DemeterDesktop = () => {
+    return (
+        <div className="demeter-desktop">
+            <div className="divbackground-radial-gradient" />
+            <div className="div">
+                <div className="div1" />
+                <div className="div2">
+                    <div className="div3">
+                        <div className="a">
+                            <div className="lend-tokens">Lend Tokens</div>
+                            <div className="div4">
+                                <div className="our-self-repaying-loans">
+                                    Our self-repaying loans earn interest on
+                                    Real World Assets
+                                </div>
+                                <div className="invest-in-rwa">lend Tokens</div>
+                            </div>
+                        </div>
+                        <div className="a1">
+                            <div className="lend-tokens">Earn Yield</div>
+                            <div className="div4">
+                                <div className="our-self-repaying-loans">
+                                    Invest in Real World Assets and earn yield
+                                </div>
+                                <div className="invest-in-rwa">
+                                    Invest in RWA
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="div6">
+                        <div className="a2">
+                            <div className="div7">
+                                <div className="borrow-crypto">
+                                    Borrow Crypto
+                                </div>
+                                {/* <img
+                                    className="image-icon"
+                                    alt=""
+                                    src="../image@2x.png"
+                                /> */}
+                            </div>
+                            <div className="div8">
+                                <div className="leverage-your-crypto">
+                                    Leverage your crypto holdings to access
+                                    liquidity in the asset of your choice.
+                                </div>
+                                <div className="invest-in-rwa">Borrow now</div>
+                            </div>
+                        </div>
+                        <div className="a3">
+                            <div className="div9">
+                                <div className="synthetic-pools">
+                                    Synthetic Pools
+                                </div>
+                                {/* <img
+                                    className="image-icon1"
+                                    alt=""
+                                    src="../image1@2x.png"
+                                /> */}
+                            </div>
+                            <div className="div10">
+                                <div className="provide-liquidity-to">
+                                    Provide liquidity to pools on Demeter and
+                                    earn fees on swaps.
+                                </div>
+                                <div className="invest-in-rwa">
+                                    Provide liquidity
+                                </div>
+                            </div>
+                        </div>
+                        <div className="a4">
+                            <div className="div11">
+                                <div className="borrow-crypto">Build dApps</div>
+                                {/* <img
+                                    className="image-icon2"
+                                    alt=""
+                                    src="../image2@2x.png"
+                                /> */}
+                            </div>
+                            <div className="div12">
+                                <div className="build-apps-and">
+                                    Build apps and tools on the most innovative
+                                    Dapp on web 3. Integrate the Demeter API and
+                                    SDK.
+                                </div>
+                                <div className="invest-in-rwa">
+                                    Developer docs
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="div13">
+                        <div className="div14">
+                            <b className="powered-by-the">
+                                Powered by the Demeter Labs protocol
+                            </b>
+                            <div className="the-worlds-most">
+                                The worlds most innovative RWA investments by a
+                                global community.
+                            </div>
+                        </div>
+                        <div className="a5">
+                            <div className="learn-more">Learn more</div>
+                        </div>
+                    </div>
+                    <div className="div15">
+                        <div className="div16">
+                            <div className="div17">
+                                {/* <img
+                                    className="svg-icon"
+                                    alt=""
+                                    src="../svg.svg"
+                                />
+                                <img
+                                    className="svg-icon"
+                                    alt=""
+                                    src="../svg1.svg"
+                                />
+                                <img
+                                    className="svg-icon"
+                                    alt=""
+                                    src="../svg2.svg"
+                                /> */}
+                            </div>
+                            <div className="demeter-labs">
+                                © 2023 Demeter Labs
+                            </div>
+                        </div>
+                        <div className="div18">
+                            <div className="div19">
+                                <b className="app">App</b>
+                                <div className="lend">Lend</div>
+                                <div className="earn">Earn</div>
+                                <div className="borrow">Borrow</div>
+                                <div className="pools">Pools</div>
+                            </div>
+                            <div className="div19">
+                                <b className="protocol">Protocol</b>
+                                <div className="community">Community</div>
+                                <div className="governance">Governance</div>
+                                <div className="developers">Developers</div>
+                            </div>
+                            <div className="div19">
+                                <b className="company">Company</b>
+                                <div className="careers">Careers</div>
+                                <div className="blog">Blog</div>
+                                <div className="bounties">Bounties</div>
+                            </div>
+                            <div className="div19">
+                                <b className="get-help">Get Help</b>
+                                <div className="contact-us">Contact Us</div>
+                                <div className="help-center">Help Center</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="div23">
+                    <div className="harvest-rwa-yield">Harvest RWA Yield</div>
+                    <div className="a6">
+                        <div className="get-started">Get started</div>
+                    </div>
+                    <div className="div24">
+                        <div className="get-started">Learn more</div>
+                        {/* <img className="svg-icon3" alt="" src="../svg3.svg" /> */}
+                    </div>
+                </div>
+                <b className="safe-liquidity-in-container">
+                    <span className="safe-liquidity-in-container1">
+                        <p className="safe-liquidity-in">Safe liquidity in</p>
+                        <p className="any-season"> any season</p>
+                    </span>
+                </b>
+                <b className="demeter-labs1">DEMETER LABS</b>
+                <div className="div-child" />
+            </div>
+            <div className="div25">
+                <div className="div26">
+                    {/* <img className="svg-icon4" alt="" src="../svg4.svg" /> */}
+                    <div className="divplaceholder">
+                        <div className="lending-and-rwa">
+                            Lending and RWA Yield
+                        </div>
+                    </div>
+                    <div className="div27">
+                        <div className="div28">/</div>
+                    </div>
+                </div>
+                <div className="div29">
+                    {/* <img className="svg-icon5" alt="" src="../svg5.svg" /> */}
+                    <div className="button">
+                        {/* <img
+                            className="image-icon3"
+                            alt=""
+                            src="../image3@2x.png"
+                        /> */}
+                        <div className="ethereum">Ethereum</div>
+                        {/* <img className="svg-icon6" alt="" src="../svg6.svg" /> */}
+                    </div>
+                    <div className="div30">
+                        <div className="get-started">Connect</div>
+                        <div className="div31" />
+                        {/* <img className="svg-icon7" alt="" src="../svg7.svg" /> */}
+                    </div>
+                </div>
+                <div className="d">D</div>
+                <div className="div32">
+                    <div className="lend1">Lend</div>
+                    <div className="earn1">Earn</div>
+                    <div className="borrow1">Borrow</div>
+                    <div className="pool">Pool</div>
+                </div>
+            </div>
+            <div className="a7">
+                <div className="lend-crypto">LEND CRYPTO</div>
+                <div className="div33">
+                    {/* <img className="div-item" alt="" /> */}
+                    <div className="lend-and-earn">
+                        Lend and earn RWA yield on Ethereum, Polygon, Optimism,
+                        and more.
+                    </div>
+                    <div className="trade-tokens">Trade Tokens</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default DemeterDesktop;
